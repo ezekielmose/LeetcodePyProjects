d7708e785467f626da72deade5e1d13778de32e6
ezekiel <ezekielmose97@gmail.com>
2023-06-07 07:42:50 +0300

    Function to add two numbers

--- Array.py
+++ Array.py
@@ -1,9 +1,26 @@
 from array import *
 
-def show(arr):
+'''def show(arr):
     for i in arr:
         print(i, end=', ')
 
 arr = array('i', [16, 27, 77, 71, 70,
                   75, 48, 19, 110])
-show(arr)
+show(arr)'''
+
+class Solution:
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num_list=dict()
+        for num in nums:
+            if not num in num_list:
+                num_list[num]=1
+            else:
+                num_list[num]+=1
+        return max(num_list, key=num_list.get)
+
+    majorityElement( nums= ['2','3','4','5','6','6','6','7'])
+
---
+++ SumofTwo.py
@@ -0,0 +1,6 @@
+def addTwo(a,b): #create function and pass its arguments
+    sums=a+b #perfom the operation
+    return sums #return the sum
+num1=10 #declate the two numbers
+num2=23
+print(addTwo(num1,num2)) #call the function
